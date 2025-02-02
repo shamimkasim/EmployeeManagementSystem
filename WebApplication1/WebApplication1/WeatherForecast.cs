4429c88261577ff8945427f3046d74811a6d6ae9
shamimkasim <shami@DESKTOP-I3CNEPH>
2025-02-02 20:34:32 -0300

    also

---
+++ WebApplication1/WebApplication1/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace WebApplication1
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public string TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
